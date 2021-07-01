471ca00d13c39152436524a4de30e3aeb51759d5
MikeYef96 <mike.yef@gmail.com>
2021-07-01 19:36:44 +0300

    test task done

--- random-strings/src/app/app.component.ts
+++ random-strings/src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { fromEvent, interval, Observable, Subject, timer } from 'rxjs';
+import { delay, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'random-strings';
+  title: string = 'random-strings';
+  textString: string = '';
+  palindrome: string = '';
+
+  clicks = fromEvent(document, 'click');
+  delayedClicks$ = this.clicks.pipe(delay(3000));
+
+  getRandomString(): void {
+    let text: string = '';
+    //here just this chars so no need to wait when it matches for whole alphabet
+    const allChars: string = 'ab0123';
+
+    for (let i = 0; i < 5; i++) {
+      text += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    }
+
+    console.dir(this.textString);
+
+    this.delayedClicks$.subscribe(() => (this.textString = text));
+    this.delayedClicks$.subscribe(
+      () => (this.palindrome = this.textString.split('').reverse().join(''))
+    );
+  }
 }
